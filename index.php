5fb6023545c6d37ccf6344f65816a0ebefb03d1b
andrea8980 <andrea@casaloda.it>
2026-08-01 17:20:21 +0200

    Refactor Movie class and update HTML to use getMovieInfo method for displaying movie details

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
 
-class Movie {
+class Movie
+{
     public $title;
     public $director;
     public $genre;
 
-function __construct($_title, $_director, $_genre) {
-    $this->title = $_title;
-    $this->director = $_director;
-    $this->genre = $_genre;
-}
+    function __construct($_title, $_director, $_genre)
+    {
+        $this->title = $_title;
+        $this->director = $_director;
+        $this->genre = $_genre;
+    }
 
-
+    public static function getMovieInfo($movie) {
+        return $movie->title . " - " . $movie->director . " - " . $movie->genre;
+    }
 }
 
 $movie1 = new Movie("Inception", "Christopher Nolan", "Sci-Fi");
@@ -24,17 +28,25 @@ $movie3 = new Movie("Interstellar", "Christopher Nolan", "Sci-Fi");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ex-php8-oop-movie</title>
 </head>
+
 <body>
     <h1>OOP</h1>
-    <ul>
-        <li><?php echo $movie1->title; ?> - <?php echo $movie1->director; ?> - <?php echo $movie1->genre; ?></li>
-        <li><?php echo $movie2->title; ?> - <?php echo $movie2->director; ?> - <?php echo $movie2->genre; ?></li>
-        <li><?php echo $movie3->title; ?> - <?php echo $movie3->director; ?> - <?php echo $movie3->genre; ?></li>
-    </ul>
+
+    <h2>Movie 1</h2>
+    <p><?php echo Movie::getMovieInfo($movie1); ?></p>
+
+    <h2>Movie 2</h2>
+    <p><?php echo Movie::getMovieInfo($movie2); ?></p>
+
+    <h2>Movie 3</h2>
+    <p><?php echo Movie::getMovieInfo($movie3); ?></p>
+    
 </body>
+
 </html>
