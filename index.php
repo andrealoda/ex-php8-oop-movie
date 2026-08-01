ab0281512264a174b2dc88374d96f8ac50a9c3d8
andrea8980 <andrea@casaloda.it>
2026-08-01 17:31:17 +0200

    Refactor Movie class to use Genre object in constructor and update getMovieInfo method

--- index.php
+++ index.php
@@ -1,28 +1,41 @@
 <?php
 
+class Genre
+{
+    public $genreName;
+
+    function __construct($_genreName)
+    {
+        $this->genreName = $_genreName;
+    }
+}
+
+
 class Movie
 {
     public $title;
     public $director;
     public $genre;
 
-    function __construct($_title, $_director, $_genre)
+    function __construct($_title, $_director, Genre $_genre)
     {
         $this->title = $_title;
         $this->director = $_director;
         $this->genre = $_genre;
     }
 
-    public static function getMovieInfo($movie) {
-        return $movie->title . " - " . $movie->director . " - " . $movie->genre;
+    public function getMovieInfo()
+    {
+        return $this->title . " - " . $this->director . " - " . $this->genre->genreName;
     }
 }
 
-$movie1 = new Movie("Inception", "Christopher Nolan", "Sci-Fi");
 
-$movie2 = new Movie("The Dark Knight", "Christopher Nolan", "Action");
+$movie1 = new Movie("Inception", "Christopher Nolan", new Genre("Sci-Fi"));
 
-$movie3 = new Movie("Interstellar", "Christopher Nolan", "Sci-Fi");
+$movie2 = new Movie("The Dark Knight", "Christopher Nolan", new Genre("Action"));
+
+$movie3 = new Movie("Interstellar", "Christopher Nolan", new Genre("Sci-Fi"));
 
 
 ?>
@@ -39,14 +52,14 @@ $movie3 = new Movie("Interstellar", "Christopher Nolan", "Sci-Fi");
     <h1>OOP</h1>
 
     <h2>Movie 1</h2>
-    <p><?php echo Movie::getMovieInfo($movie1); ?></p>
+    <p><?php echo $movie1->getMovieInfo(); ?></p>
 
     <h2>Movie 2</h2>
-    <p><?php echo Movie::getMovieInfo($movie2); ?></p>
+    <p><?php echo $movie2->getMovieInfo(); ?></p>
 
     <h2>Movie 3</h2>
-    <p><?php echo Movie::getMovieInfo($movie3); ?></p>
-    
+    <p><?php echo $movie3->getMovieInfo(); ?></p>
+
 </body>
 
 </html>
