6c7110e9ee34d908ea232aae80b94747089e1fbe
andrea8980 <andrea@casaloda.it>
2026-08-01 17:17:06 +0200

    Add movie3 instance and update HTML to display all movie details

--- index.php
+++ index.php
@@ -18,6 +18,8 @@ $movie1 = new Movie("Inception", "Christopher Nolan", "Sci-Fi");
 
 $movie2 = new Movie("The Dark Knight", "Christopher Nolan", "Action");
 
+$movie3 = new Movie("Interstellar", "Christopher Nolan", "Sci-Fi");
+
 
 ?>
 <!DOCTYPE html>
@@ -30,8 +32,9 @@ $movie2 = new Movie("The Dark Knight", "Christopher Nolan", "Action");
 <body>
     <h1>OOP</h1>
     <ul>
-        <li><?php echo $movie1->title; ?></li>
-        <li><?php echo $movie2->title; ?></li>
+        <li><?php echo $movie1->title; ?> - <?php echo $movie1->director; ?> - <?php echo $movie1->genre; ?></li>
+        <li><?php echo $movie2->title; ?> - <?php echo $movie2->director; ?> - <?php echo $movie2->genre; ?></li>
+        <li><?php echo $movie3->title; ?> - <?php echo $movie3->director; ?> - <?php echo $movie3->genre; ?></li>
     </ul>
 </body>
 </html>
